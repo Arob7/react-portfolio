50f1e1911168e6915e89b2ec642a0106f8860d29
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-11-08 20:47:39 -0500

    added images and background color to home & portfolio pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,31 @@
 import React, { useReducer, useRef } from "react";
 import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
+import "../App.css";
+// import Jumbotron from "react-bootstrap/Jumbotron";
 
 function Home() {
   return (
-    <div className="container text-center">
-      <Card>
-        <Card.Body>
-          <img src={require("../images/profile.jpg")}></img>
-          Hello! My name is Arion Robinson. I am attending Michigan State
-          University's Full Stack Web Development Boot Camp and graduate
-          November 2020. I have held a variety of positions, my passion is
-          creating new things and challenging myself. <br></br>I want enhance my
-          skills through continuous learning, self-development and creativity.
-        </Card.Body>
-      </Card>
+    <div className="home">
+      <div className="container text-center">
+        <Card>
+          <Card.Body>
+            <Image
+              className="photo"
+              src={require("../images/profile.jpg")}
+              roundedCircle
+            />
+            <br></br>
+            Hello! My name is Arion Robinson.
+            <br></br>I am attending Michigan State University's Full Stack Web
+            Development Boot Camp and graduate November 2020.
+            <br></br>I have held a variety of positions, my passion is creating
+            new things and challenging myself.
+            <br></br>I want enhance my skills through continuous learning,
+            self-development and creativity.
+          </Card.Body>
+        </Card>
+      </div>
     </div>
   );
 }
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,22 @@
 import React, { useReducer, useRef } from "react";
+import Image from "react-bootstrap/Image";
+import "../index.css";
+// import Card from "react-bootstrap/Card";
 
 function Portfolio() {
-  return <div className="container text-center">PORTFOLIO</div>;
+  return (
+    <div className="portfolio">
+      <div className="container text-center">
+        <Image src={require("../images/dailyplannerscreenshot.png")} fluid />
+        <div></div>
+        <Image src={require("../images/appscreenshot.JPG")} fluid />
+        <br></br>
+        <Image src={require("../images/passwordscreenshot.PNG")} fluid />
+        <br></br>
+        <Image src={require("../images/project1screenshot.png")} fluid />
+      </div>
+    </div>
+  );
 }
 
 export default Portfolio;
