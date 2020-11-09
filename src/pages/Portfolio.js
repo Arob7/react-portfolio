14da4caa08c3743cbfc6270608ff83de43b44613
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-11-08 22:32:18 -0500

    added btn links on portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,19 +1,100 @@
 import React, { useReducer, useRef } from "react";
 import Image from "react-bootstrap/Image";
 import "../index.css";
-// import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 function Portfolio() {
   return (
     <div className="portfolio">
       <div className="container text-center">
         <Image src={require("../images/dailyplannerscreenshot.png")} fluid />
-        <div></div>
+        <Button
+          variant="dark"
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href = "https://github.com/Arob7/Daily-Planner.git";
+          }}
+        >
+          GITHUB
+        </Button>{" "}
+        <Button
+          variant="dark"
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href = "https://arob7.github.io/Daily-Planner/";
+          }}
+        >
+          WEBSITE
+        </Button>{" "}
         <Image src={require("../images/appscreenshot.JPG")} fluid />
-        <br></br>
+        <Button
+          variant="dark"
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href =
+              "https://github.com/DeMarcoHeath/project2.git";
+          }}
+        >
+          GITHUB
+        </Button>{" "}
+        <Button
+          variant="dark"
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href =
+              "https://sheltered-temple-43461.herokuapp.com/";
+          }}
+        >
+          WEBSITE
+        </Button>{" "}
         <Image src={require("../images/passwordscreenshot.PNG")} fluid />
-        <br></br>
+        <Button
+          variant="dark"
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href =
+              "https://github.com/Arob7/password-generator.git";
+          }}
+        >
+          GITHUB
+        </Button>{" "}
+        <Button
+          variant="dark"
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href =
+              "https://arob7.github.io/password-generator/";
+          }}
+        >
+          WEBSITE
+        </Button>{" "}
         <Image src={require("../images/project1screenshot.png")} fluid />
+        <Button
+          variant="dark"
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href = "https://github.com/Scottiss/project-1.git";
+          }}
+        >
+          GITHUB
+        </Button>{" "}
+        <Button
+          variant="dark"
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.href = "https://scottiss.github.io/project-1";
+          }}
+        >
+          WEBSITE
+        </Button>{" "}
       </div>
     </div>
   );
