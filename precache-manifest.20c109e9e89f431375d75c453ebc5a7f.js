7afb76a1fb67567e1f8815710ac001a6d996ece3
Arob7 <66214652+Arob7@users.noreply.github.com>
2020-11-17 21:26:09 -0500

    Updates

---
+++ precache-manifest.20c109e9e89f431375d75c453ebc5a7f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960c72db4d0bfd816c28a566458fd4f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f21f347e730cdb44b109",
+    "url": "/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "aee4d438ebee84783e07",
+    "url": "/static/css/main.67892a26.chunk.css"
+  },
+  {
+    "revision": "f21f347e730cdb44b109",
+    "url": "/static/js/2.bd8ab300.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.bd8ab300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aee4d438ebee84783e07",
+    "url": "/static/js/main.15b6bbcf.chunk.js"
+  },
+  {
+    "revision": "01de8b8c28e818a945d1",
+    "url": "/static/js/runtime-main.bf7d2e50.js"
+  },
+  {
+    "revision": "950a7ae8dc36bf7a77d92d51a79a1fc5",
+    "url": "/static/media/appscreenshot.950a7ae8.JPG"
+  },
+  {
+    "revision": "c49da5a22851b7852989c07b217458a4",
+    "url": "/static/media/budgetscreenshot.c49da5a2.JPG"
+  },
+  {
+    "revision": "3fa81d3d954a88791fc4ae59c8adda4b",
+    "url": "/static/media/dailyplannerscreenshot.3fa81d3d.png"
+  },
+  {
+    "revision": "70993132e4f8c1557eae25ced56812a2",
+    "url": "/static/media/fitnesstracker.70993132.JPG"
+  },
+  {
+    "revision": "98e9fe671f86c246a4152f0c449be7f3",
+    "url": "/static/media/passwordscreenshot.98e9fe67.PNG"
+  },
+  {
+    "revision": "edb4fc5402cc465147797fd6441889da",
+    "url": "/static/media/profile.edb4fc54.jpg"
+  },
+  {
+    "revision": "b3f3cd37d02fc84cd5799cb8d7cffe24",
+    "url": "/static/media/project1screenshot.b3f3cd37.png"
+  }
+]);
